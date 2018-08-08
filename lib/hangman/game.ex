cb09b84958b258cf080bbaf9b517ee73023e6da1
Dennis Zelada <denniszelada@gmail.com>
2018-08-08 10:14:37 +0000

    Add make move funciton and tests

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -3,7 +3,7 @@ defmodule Hangman.Game do
   defstruct(
       turns_left: 7,
       game_state: :initializing,
-      letters:    [],
+      letters:    []
   )
 
   def new_game() do
@@ -11,4 +11,13 @@ defmodule Hangman.Game do
       letters: Dictionary.random_word |> String.codepoints 
     }
   end
+
+  def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
+    { game, tally(game) }    
+  end
+
+  def tally(game) do
+    123
+  end
+
 end 
